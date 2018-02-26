ee34e7a996a2a166ac5f4f7bed28d6de5eac8630
ruboi <ruboi@smile.fr>
2018-02-26 20:47:58 +0200

    Homework #1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// formatted function
+(function(win){
+    var params = {states:{
+            url:"/",
+            template: "temlate.js"
+        }
+    };
+    function setStates(params){
+        if(win && !win.params){
+            win.params = params;
+        }
+    }
+    setStates();
+    console.log(params.states.template);
+})
+(window);
